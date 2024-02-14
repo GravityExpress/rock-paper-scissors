57d34a5b4d3f2a53f4be09caab72dd03d9a112aa
GravityExpress <102402506+GravityExpress@users.noreply.github.com>
2024-02-14 22:11:44 +0800

    added outline for the program in script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Gets computer's choice
+function getComputerChoice()
+{
+    // Randomly select rock, paper, or scissors
+}
+
+// Plays a round of rock paper scissors
+function playRound(playerSelection, computerSelection)
+{
+    // Play a single round
+}
+
+// Start a game of rock paper scissors
+function playGame()
+{
+    // Play a game of rock paper scissors
+}
