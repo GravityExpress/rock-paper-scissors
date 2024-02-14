f9c4c83d960464642d1e003c708ce15d2696d231
GravityExpress <102402506+GravityExpress@users.noreply.github.com>
2024-02-14 23:38:40 +0800

    added playGame functionality

--- script.js
+++ script.js
@@ -15,9 +15,20 @@ function getComputerChoice()
     }
 }
 
+function getPlayerChoice()
+{
+    let choice = prompt("What hand would you like to throw?");
+    return choice;
+}
+
 // Plays a round of rock paper scissors
 function playRound(playerSelection, computerSelection)
 {
+    // Capitalizes a word
+    capitalize = word => {
+        return word.substr(0,1).toUpperCase().concat(word.substr(1).toLowerCase());
+    };
+
     // Play a single round
     if (playerSelection.toLowerCase() !== "rock" && playerSelection.toLowerCase() !== "paper"  && playerSelection.toLowerCase() !== "scissors")
     {
@@ -67,8 +78,15 @@ function playRound(playerSelection, computerSelection)
 function playGame()
 {
     // Play a game of rock paper scissors
+    alert("This is a game of Rock, Paper, and Scissors\n\nPlay against the computer by choosing between: \nRock, Paper, and Scissors");
+    let rounds = Number(prompt("How many rounds would you like to play?"));
+    let currentRound = 0
+    
+    while (currentRound < rounds)
+    {
+        playRound(getPlayerChoice(), getComputerChoice());
+        currentRound++;
+    }
 }
 
-capitalize = word => {
-    return word.substr(0,1).toUpperCase().concat(word.substr(1).toLowerCase());
-};
+playGame();
